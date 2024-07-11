fc43273e6efbbb382eeae3b8bfc8c274c1fddc61
Gaurav945572 <gauravoppo9455@gmail.com>
2024-07-11 19:20:10 +0530

    MongoDB connected

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+
+mongoose.connect(process.env.key).then(()=>{
+    console.log("mongoDB connected");
+}).catch((error)=>{
+    console.log(error)
+});
 const app = express();
-
 
 
 app.listen(3000,()=>{
